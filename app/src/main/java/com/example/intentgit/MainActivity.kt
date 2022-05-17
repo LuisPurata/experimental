204e3b73d41aada4bd97f7e2ef160f7cac35e65e
CEBSA <ninguno>
2022-05-17 17:07:00 -0500

    Ultima actualizacion 2

--- app/src/main/java/com/example/intentgit/MainActivity.kt
+++ app/src/main/java/com/example/intentgit/MainActivity.kt
@@ -7,8 +7,13 @@ class MainActivity : AppCompatActivity() {
 
     var variable = "Hello World"
     var asdfs = "Como Estas Mundo Cruel"
+    var v1 = 1
+    var v2 = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var resultado = v1 + v2
+
     }
 }
