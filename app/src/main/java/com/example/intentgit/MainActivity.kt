0f5ef5229ac17428aff34ad42bb3356daeb4269c
CEBSA <ninguno>
2022-05-18 12:26:39 -0500

    Ultima actualizacion 18 / 05 / 2020

--- app/src/main/java/com/example/intentgit/MainActivity.kt
+++ app/src/main/java/com/example/intentgit/MainActivity.kt
@@ -15,10 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Inicializar Variables
         val button: Button = findViewById(R.id.boton_principal)
         val button2: Button = findViewById(R.id.buton_secundario)
         val text1 : TextView = findViewById(R.id.text1)
         val text2 : TextView = findViewById(R.id.text2)
+        //-------------------------------------------------------
+
         button.visibility = View.GONE
         button.setOnClickListener{
             text1.visibility=View.VISIBLE //Ocultar algun elemento
