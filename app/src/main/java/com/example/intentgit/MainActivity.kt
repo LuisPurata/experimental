4dcbeedeb8b4af82f5538d10ac3a01ac286183c1
CEBSA <ninguno>
2022-05-18 11:25:41 -0500

    Ultima actualizacion 18 / 05 / 2020

--- app/src/main/java/com/example/intentgit/MainActivity.kt
+++ app/src/main/java/com/example/intentgit/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.intentgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button: Button = findViewById(R.id.boton_principal)
+        val button2: Button = findViewById(R.id.buton_secundario)
+        val text1 : TextView = findViewById(R.id.text1)
+        val text2 : TextView = findViewById(R.id.text2)
+
+        button.setOnClickListener{
+            text1.visibility=View.GONE
+        }
+
+        button2.setOnClickListener {
+            text1.visibility = View.VISIBLE
+        }
 
     }
 }
