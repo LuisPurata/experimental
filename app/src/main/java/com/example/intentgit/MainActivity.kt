cec7324527c935e0552270e13ea0b135025e7cfe
CEBSA <ninguno>
2022-05-18 17:55:02 -0500

    Ultima actualizacion 18 / 05 / 2020

--- app/src/main/java/com/example/intentgit/MainActivity.kt
+++ app/src/main/java/com/example/intentgit/MainActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
+import com.android.volley.toolbox.StringRequest
 
 
 class MainActivity : AppCompatActivity() {
 
+
+
+    lateinit var edittext_usuario: EditText
+    lateinit var edittext_contrasena: EditText
+    lateinit var button_iniciar: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,24 +22,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Inicializar Variables
-        val button: Button = findViewById(R.id.boton_principal)
+        val button_iniciar: Button = findViewById(R.id.boton_principal)
         val button2: Button = findViewById(R.id.buton_secundario)
-        val text1 : TextView = findViewById(R.id.text1)
-        val text2 : TextView = findViewById(R.id.text2)
+        val edittext_usuario : EditText = findViewById(R.id.text1)
+        val edittext_contrasena : EditText = findViewById(R.id.text2)
+
         //-------------------------------------------------------
 
-        button.visibility = View.GONE
-        button.setOnClickListener{
-            text1.visibility=View.VISIBLE //Ocultar algun elemento
+        button_iniciar.visibility = View.GONE
+        button_iniciar.setOnClickListener{
+            edittext_usuario.visibility=View.VISIBLE //Ocultar algun elemento
             button2.visibility = View.VISIBLE
-            button.visibility = View.GONE
+            button_iniciar.visibility = View.GONE
+
         }
 
         button2.setOnClickListener {
-            text1.visibility = View.GONE //Mostrar algun elemento
-            button.visibility = View.VISIBLE
+            edittext_usuario.visibility = View.GONE //Mostrar algun elemento
+            button_iniciar.visibility = View.VISIBLE
             button2.visibility = View.GONE
         }
 
+    }
+
+    fun validarUsuario(URL: String){
+        var stringRequest: StringRequest = StringRequest()
     }
 }
